b58e90118c3ccd7eb5b811a629842002605c24a8
Jessada Thutkawkorapin <jessada@milou1.uppmax.uu.se>
2014-03-06 10:52:23 +0100

    restructure scripts so that global are splited into user side and application/library side

--- scripts/chr9_linkage/gen_consensus_xls_for_chr9_linkage_uppsala.sh
+++ scripts/chr9_linkage/gen_consensus_xls_for_chr9_linkage_uppsala.sh
@@ -1,42 +1,32 @@
 #!/bin/bash
 
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-source ~/.bashrc
-
-dataset_name="chr9_linkage_uppsala"
-working_dir=$CMM_PROJECTS_WORKING_DIR
-
 project_name="chr9_linkage"
 sub_project_name="uppsala"
+dataset_name="chr9_linkage_uppsala"
 
-#vcf_region="9:92298000-105323000"
-
-if [ ! -d "$CMM_PROJECTS_OUTPUT_DIR/$project_name" ]; then
-    mkdir "$CMM_PROJECTS_OUTPUT_DIR/$project_name"
-fi
 if [ ! -z "$sub_project_name" ]; then
-    out_dir="$CMM_PROJECTS_OUTPUT_DIR/$project_name/$sub_project_name"
-    if [ ! -d "$out_dir" ]; then
-        mkdir "$out_dir"
-    fi
+    in_dir="$CMM_PROJECTS_OUTPUT_DIR/$project_name/$sub_project_name"
 else
-    out_dir="$CMM_PROJECTS_OUTPUT_DIR/$project_name"
+    in_dir="$CMM_PROJECTS_OUTPUT_DIR/$project_name"
 fi
 
-oaf_in_file=$out_dir/$dataset_name.oaf
-#gt_vcf_gt_in_file=$out_dir/$dataset_name.gt.vgt
-mt_vcf_gt_in_file=$out_dir/$dataset_name.mt.vgt
-sa_in_file=$out_dir/$dataset_name.sa
+
+##********************************************* set parameters here *****************************************************
+export CONSENSUS_XLS_RUNNING_KEY=$dataset_name
+export CONSENSUS_XLS_WORKING_DIR=$CMM_PROJECTS_WORKING_DIR
+export CONSENSUS_XLS_OUT_DIR=$in_dir
+
+export CONSENSUS_XLS_OAF_IN_FILE=$in_dir/$dataset_name.oaf
+#export CONSENSUS_XLS_GT_VCF_GT_IN_FILE=$in_dir/$dataset_name.gt.vgt
+export CONSENSUS_XLS_MT_VCF_GT_IN_FILE=$in_dir/$dataset_name.mt.vgt
+export CONSENSUS_XLS_SA_IN_FILE=$in_dir/$dataset_name.sa
+
+# vcf region from rs10124153 to rs7040414
+export CONSENSUS_XLS_VCF_REGION="9:104030000-104060000"
+
+export CONSENSUS_XLS_OAF_RATIO="0.2"
+export CONSENSUS_XLS_MAF_RATIO="0.2"
+##********************************************* set parameters here *****************************************************
 
 
-cmd="$SCRIPT_GEN_CONSENSUS_XLS -k $dataset_name -O $oaf_in_file -S $sa_in_file -w $working_dir -o $out_dir"
-if [ ! -z "$gt_vcf_gt_in_file" ]; then
-    cmd+=" -G $gt_vcf_gt_in_file"
-fi
-if [ ! -z "$mt_vcf_gt_in_file" ]; then
-    cmd+=" -M $mt_vcf_gt_in_file"
-fi
-
-eval $cmd
-
+$EXEC_SCRIPT_GEN_CONSENSUS_XLS
