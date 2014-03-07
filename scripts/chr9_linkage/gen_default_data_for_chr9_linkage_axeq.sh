d1cba475d91ab10aac60540a8754bc1d5360b1fc
Jessada Thutkawkorapin <jessada@milou2.uppmax.uu.se>
2014-03-07 13:56:15 +0100

    done template project to exec scripts as users

--- scripts/chr9_linkage/gen_default_data_for_chr9_linkage_axeq.sh
+++ scripts/chr9_linkage/gen_default_data_for_chr9_linkage_axeq.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-source ~/.bashrc
-
-project_code="b2012247"
-dataset_name="chr9_linkage_axeq"
-tabix_file=$CMM_AXEQ_CHR9_ALL_PATIENTS_GZ
-working_dir=$CMM_PROJECTS_WORKING_DIR
-
 project_name="chr9_linkage"
 sub_project_name="axeq"
-
-vcf_region="9:92298000-105323000"
-col_names="24-Co166,24-Co213,24-Co648,8-Co37,275-Co618,478-Co1274,296-Co876"
+dataset_name="chr9_linkage_axeq"
 
 if [ ! -d "$CMM_PROJECTS_OUTPUT_DIR/$project_name" ]; then
     mkdir "$CMM_PROJECTS_OUTPUT_DIR/$project_name"
@@ -27,30 +16,26 @@ else
     out_dir="$CMM_PROJECTS_OUTPUT_DIR/$project_name"
 fi
 
-oaf_out_file=$out_dir/$dataset_name.oaf
-gt_vcf_gt_out_file=$out_dir/$dataset_name.gt.vgt
-mt_vcf_gt_out_file=$out_dir/$dataset_name.mt.vgt
-sa_out_file=$out_dir/$dataset_name.sa
 
-cmd="$SCRIPT_GEN_DEFAULT_DATA -p $project_code -N $dataset_name -t $tabix_file -w $working_dir"
-if [ ! -z "$oaf_out_file" ]; then
-    cmd+=" -O $oaf_out_file"
-fi
-if [ ! -z "$gt_vcf_gt_out_file" ]; then
-    cmd+=" -G $gt_vcf_gt_out_file"
-fi
-if [ ! -z "$mt_vcf_gt_out_file" ]; then
-    cmd+=" -M $mt_vcf_gt_out_file"
-fi
-if [ ! -z "$sa_out_file" ]; then
-    cmd+=" -S $sa_out_file"
-fi
-if [ ! -z "$vcf_region" ]; then
-    cmd+=" -R $vcf_region"
-fi
-if [ ! -z "$col_names" ]; then
-    cmd+=" -c $col_names"
-fi
-eval $cmd
+##********************************************* set parameters here *****************************************************
+export DEFAULT_DATA_PROJECT_CODE="b2012247"
+export DEFAULT_DATA_DATASET_NAME=$dataset_name
+export DEFAULT_DATA_TABIX_FILE=$CMM_AXEQ_CHR9_ALL_PATIENTS_GZ
+export DEFAULT_DATA_HBVDB_TOOLS_ROOT_DIR=$HBVDB_TOOLS_ROOT_DIR
+export DEFAULT_DATA_ANNOVAR_ROOT_DIR=$ANNOVAR_ROOT_DIR
+export DEFAULT_DATA_WORKING_DIR=$CMM_PROJECTS_WORKING_DIR
+export DEFAULT_DATA_LOG_DIR=$CMM_PROJECTS_LOG_DIR
 
+export DEFAULT_DATA_OAF_OUT_FILE=$out_dir/$dataset_name.oaf
+export DEFAULT_DATA_GT_VCF_GT_OUT_FILE=$out_dir/$dataset_name.gt.vgt
+export DEFAULT_DATA_MT_VCF_GT_OUT_FILE=$out_dir/$dataset_name.mt.vgt
+export DEFAULT_DATA_SA_OUT_FILE=$out_dir/$dataset_name.sa
 
+#export DEFAULT_DATA_PROJECT_NAME="chr9_linkage"
+#export DEFAULT_DATA_SUB_PROJECT_NAME="uppsala"
+
+export DEFAULT_DATA_VCF_REGION="9:92298000-105323000"
+export DEFAULT_DATA_COL_NAMES="24-Co166,24-Co213,24-Co648,8-Co37,275-Co618,478-Co1274,296-Co876"
+##********************************************* set parameters here *****************************************************
+
+$EXEC_SCRIPT_GEN_DEFAULT_DATA
--- scripts/chr9_linkage/gen_default_data_for_chr9_linkage_uppsala.sh
+++ scripts/chr9_linkage/gen_default_data_for_chr9_linkage_uppsala.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-source ~/.bashrc
-
-project_code="b2012247"
-dataset_name="chr9_linkage_uppsala"
-tabix_file=$CMM_UPPSALA_50FAM_ALL_PATIENTS_GZ
-working_dir=$CMM_PROJECTS_WORKING_DIR
-
 project_name="chr9_linkage"
 sub_project_name="uppsala"
-
-vcf_region="9:92298000-105323000"
-col_names="8-Co35,8-Co37,275-Co1262,275-Co618,478-Co1274,478-Co1207,740-1373,740-602-05o,740-1383,350-1104-03D,350-Co866,918-134-06,918-354-06,13-Co95,296-Co793,296-Co876"
+dataset_name="chr9_linkage_uppsala"
 
 if [ ! -d "$CMM_PROJECTS_OUTPUT_DIR/$project_name" ]; then
     mkdir "$CMM_PROJECTS_OUTPUT_DIR/$project_name"
@@ -27,30 +16,26 @@ else
     out_dir="$CMM_PROJECTS_OUTPUT_DIR/$project_name"
 fi
 
-oaf_out_file=$out_dir/$dataset_name.oaf
-gt_vcf_gt_out_file=$out_dir/$dataset_name.gt.vgt
-mt_vcf_gt_out_file=$out_dir/$dataset_name.mt.vgt
-sa_out_file=$out_dir/$dataset_name.sa
 
-cmd="$SCRIPT_GEN_DEFAULT_DATA -p $project_code -N $dataset_name -t $tabix_file -w $working_dir"
-if [ ! -z "$oaf_out_file" ]; then
-    cmd+=" -O $oaf_out_file"
-fi
-if [ ! -z "$gt_vcf_gt_out_file" ]; then
-    cmd+=" -G $gt_vcf_gt_out_file"
-fi
-if [ ! -z "$mt_vcf_gt_out_file" ]; then
-    cmd+=" -M $mt_vcf_gt_out_file"
-fi
-if [ ! -z "$sa_out_file" ]; then
-    cmd+=" -S $sa_out_file"
-fi
-if [ ! -z "$vcf_region" ]; then
-    cmd+=" -R $vcf_region"
-fi
-if [ ! -z "$col_names" ]; then
-    cmd+=" -c $col_names"
-fi
-eval $cmd
+##********************************************* set parameters here *****************************************************
+export DEFAULT_DATA_PROJECT_CODE="b2011117"
+export DEFAULT_DATA_DATASET_NAME=$dataset_name
+export DEFAULT_DATA_TABIX_FILE=$CMM_UPPSALA_58FAM_ALL_PATIENTS_GZ
+export DEFAULT_DATA_HBVDB_TOOLS_ROOT_DIR=$HBVDB_TOOLS_ROOT_DIR
+export DEFAULT_DATA_ANNOVAR_ROOT_DIR=$ANNOVAR_ROOT_DIR
+export DEFAULT_DATA_WORKING_DIR=$CMM_PROJECTS_WORKING_DIR
+export DEFAULT_DATA_LOG_DIR=$CMM_PROJECTS_LOG_DIR
 
+export DEFAULT_DATA_OAF_OUT_FILE=$out_dir/$dataset_name.oaf
+export DEFAULT_DATA_GT_VCF_GT_OUT_FILE=$out_dir/$dataset_name.gt.vgt
+export DEFAULT_DATA_MT_VCF_GT_OUT_FILE=$out_dir/$dataset_name.mt.vgt
+export DEFAULT_DATA_SA_OUT_FILE=$out_dir/$dataset_name.sa
 
+#export DEFAULT_DATA_PROJECT_NAME="chr9_linkage"
+#export DEFAULT_DATA_SUB_PROJECT_NAME="uppsala"
+
+export DEFAULT_DATA_VCF_REGION="9:92298000-105323000"
+export DEFAULT_DATA_COL_NAMES="8-Co35,8-Co37,275-Co1262,275-Co618,478-Co1274,478-Co1207,740-1373,740-602-05o,740-1383,358-1104-03D,358-Co866,918-134-06,918-354-06,13-Co95,296-Co793,296-Co876"
+##********************************************* set parameters here *****************************************************
+
+$EXEC_SCRIPT_GEN_DEFAULT_DATA
