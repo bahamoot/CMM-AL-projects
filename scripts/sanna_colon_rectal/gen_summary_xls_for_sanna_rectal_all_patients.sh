ae5e8bfb32dd77c3c9840fa9fad1a34a3c8d0683
Jessada Thutkawkorapin <jessada@milou1.uppmax.uu.se>
2014-04-25 12:46:59 +0200

    add script to generate excel sheet for sanna project

---
+++ scripts/sanna_colon_rectal/gen_summary_xls_for_sanna_rectal_all_patients.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+project_name="sanna_colon_rectal"
+sub_project_name="rectal_all_patients"
+dataset_name="sv_rectal_all_patients"
+
+if [ ! -z "$sub_project_name" ]; then
+    in_dir="$CMM_PROJECTS_OUTPUT_DIR/$project_name/$sub_project_name"
+else
+    in_dir="$CMM_PROJECTS_OUTPUT_DIR/$project_name"
+fi
+
+
+##********************************************* set parameters here *****************************************************
+export SUMMARY_XLS_RUNNING_KEY=$dataset_name
+export SUMMARY_XLS_WORKING_DIR=$CMM_PROJECTS_WORKING_DIR
+export SUMMARY_XLS_OUT_DIR=$in_dir
+
+export SUMMARY_XLS_OAF_IN_FILE=$in_dir/$dataset_name.oaf
+#export SUMMARY_XLS_GT_VCF_GT_IN_FILE=$in_dir/$dataset_name.gt.vgt
+export SUMMARY_XLS_MT_VCF_GT_IN_FILE=$in_dir/$dataset_name.mt.vgt
+export SUMMARY_XLS_SA_IN_FILE=$in_dir/$dataset_name.sa
+
+## vcf region from rs10124153 to rs7040414
+#export SUMMARY_XLS_VCF_REGION="9:104030000-104060000"
+
+export SUMMARY_XLS_EXONIC_FILTERING="Off"
+export SUMMARY_XLS_MISSENSE_FILTERING="Off"
+export SUMMARY_XLS_DELETERIOUS_FILTERING="Off"
+#export SUMMARY_XLS_RARE_FILTERING="On"
+
+export SUMMARY_XLS_OAF_RATIO="0.2"
+export SUMMARY_XLS_MAF_RATIO="0.2"
+##********************************************* set parameters here *****************************************************
+
+$EXEC_SCRIPT_GEN_SUMMARY_XLS
