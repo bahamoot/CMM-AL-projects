1c2b8bf01a61ff6f56b4b7d230d38eecabf8d92f
Jessada Thutkawkorapin <jessada@milou1.uppmax.uu.se>
2014-05-07 08:57:57 +0200

    done general PLINK research

---
+++ scripts/RnD/plink/generate_pheno.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+awk -F'\t' '{
+if ($4 == "Yes" && $3 == "Colon") 
+    printf "%s\t%s\t2\n", $1, $2;
+else if ($3 == "None") 
+    printf "%s\t%s\t1\n", $1, $2;
+}' $CMM_GENOTYPING_UPPSALA_PHENOTYPE
+
