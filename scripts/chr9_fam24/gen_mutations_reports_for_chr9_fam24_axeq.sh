837c77fe195a6123030e7e2bc674d5dca17d2ed7
Jessada Thutkawkorapin <jessada@milou1.uppmax.uu.se>
2014-06-27 14:50:28 +0200

    adjust scripts to compat with new version of mutations and PLINK reports

---
+++ scripts/chr9_fam24/gen_mutations_reports_for_chr9_fam24_axeq.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+project_name="chr9_fam24"
+sub_project_name="mut_reps"
+
+if [ ! -d "$CMM_PROJECTS_OUTPUT_DIR/$project_name" ]; then
+    mkdir "$CMM_PROJECTS_OUTPUT_DIR/$project_name"
+fi
+if [ ! -z "$sub_project_name" ]; then
+    project_dir="$CMM_PROJECTS_OUTPUT_DIR/$project_name/$sub_project_name"
+    running_key="$project_name"_"$sub_project_name"
+    if [ ! -d "$project_dir" ]; then
+        mkdir "$project_dir"
+    fi
+else
+    project_dir="$CMM_PROJECTS_OUTPUT_DIR/$project_name"
+    running_key="$project_name"
+fi
+
+
+##********************************************* set parameters here *****************************************************
+export MUTATIONS_REPORTS_PROJECT_CODE="b2012247"
+export MUTATIONS_REPORTS_RUNNING_KEY=$running_key
+export MUTATIONS_REPORTS_TABIX_FILE=$CMM_AXEQ_CHR9_ALL_PATIENTS_GZ
+export MUTATIONS_REPORTS_ANNOVAR_ROOT_DIR=$ANNOVAR_ROOT_DIR
+export MUTATIONS_REPORTS_PROJECT_DIR=$project_dir
+export MUTATIONS_REPORTS_LOG_DIR=$CMM_PROJECTS_LOG_DIR
+
+#cache_dir="$project_dir/cache"
+#export MUTATIONS_REPORTS_CACHE_DIR="$cache_dir"
+
+## vcf region from rs10124153 to rs7040414
+export MUTATIONS_REPORTS_VCF_REGION="9:98318926-105545483"
+#export MUTATIONS_REPORTS_COL_NAMES="185-Co603,185-Co669,312-Co1116,350-1104-03D,350-Co866,650-398-05o,650-729-05o,1290-Co1723"
+export MUTATIONS_REPORTS_FAMILIES_INFO="24:24-Co213,24-Co166"
+
+export MUTATIONS_REPORTS_EXONIC_FILTERING="Off"
+export MUTATIONS_REPORTS_MISSENSE_FILTERING="Off"
+export MUTATIONS_REPORTS_DELETERIOUS_FILTERING="Off"
+#export MUTATIONS_REPORTS_RARE_FILTERING="On"
+
+export MUTATIONS_REPORTS_OAF_RATIO="0.2"
+export MUTATIONS_REPORTS_MAF_RATIO="0.2"
+##********************************************* set parameters here *****************************************************
+
+$EXEC_SCRIPT_GEN_MUTATIONS_REPORTS
--- scripts/chr9_fam24/gen_plink_report_for_chr9_fam24.sh
+++ scripts/chr9_fam24/gen_plink_report_for_chr9_fam24.sh
@@ -9,34 +9,37 @@ if [ ! -d "$CMM_PROJECTS_OUTPUT_DIR/$project_name" ]; then
     mkdir "$CMM_PROJECTS_OUTPUT_DIR/$project_name"
 fi
 if [ ! -z "$sub_project_name" ]; then
-    out_dir="$CMM_PROJECTS_OUTPUT_DIR/$project_name/$sub_project_name"
+    project_dir="$CMM_PROJECTS_OUTPUT_DIR/$project_name/$sub_project_name"
     dataset_name="$project_name"_"$sub_project_name"
-    if [ ! -d "$out_dir" ]; then
-        mkdir "$out_dir"
+    if [ ! -d "$project_dir" ]; then
+        mkdir "$project_dir"
     fi
 else
-    out_dir="$CMM_PROJECTS_OUTPUT_DIR/$project_name"
+    project_dir="$CMM_PROJECTS_OUTPUT_DIR/$project_name"
     dataset_name="$project_name"
 fi
 
+
 ##********************************************* set parameters here *****************************************************
+export PLINK_REPORTS_PROJECT_CODE="b2011097"
+export PLINK_REPORTS_TOTAL_RUN_TIME="7-00:00:00"
 export PLINK_REPORTS_RUNNING_KEY="$dataset_name"
 export PLINK_REPORTS_PLINK_BIN_FILE_PREFIX="$CMM_GENOTYPING_UPPSALA_PROCESSED_PLINK_BIN_FILE_PREFIX"
 export PLINK_REPORTS_PLINK_PHENO_FILE="$CMM_GENOTYPING_UPPSALA_COLON_300CTRLS_WO_RELATIVES_PHENO"
 export PLINK_REPORTS_FAMILIES_HAPLOTYPES_PLINK_TFILE_PREFIX="$CMM_HAPLOTYPE_CHR9_LKG_PLINK_FILE_PREFIX"
-export PLINK_REPORTS_TFAM_INDIVIDUAL_IDS="new_fam24_shared_only"
+export PLINK_REPORTS_TFAM_FAMILY_IDS="8,275,478,740,350,918,24"
 #export PLINK_REPORTS_TFAM_INDIVIDUAL_IDS="fam_8,fam_275,fam_478,fam_740,fam_350,fam_918,new_fam24_shared_only"
 #export PLINK_REPORTS_PLINK_HAP_WINDOW_SIZES="1,5,10,20,50"
+export PLINK_REPORTS_SPECIAL_FAMILIES_INFO="24:GOLD"
 export PLINK_REPORTS_PLINK_HAP_WINDOW_SIZES="1,3,5,10,20,50,100"
 export PLINK_REPORTS_PVALUE_SIGNIFICANCE_RATIO="2e-04"
 export PLINK_REPORTS_WORKING_DIR="$CMM_PROJECTS_WORKING_DIR"
-export PLINK_REPORTS_OUT_DIR="$out_dir"
+export PLINK_REPORTS_PROJECT_DIR=$project_dir
 export PLINK_REPORTS_LOG_DIR="$CMM_PROJECTS_LOG_DIR"
 
-cache_dir="$script_dir/cache/9_98318926_105545483"
+#cache_dir="$project_dir/cache/9_98318926_105545483_old"
 #export PLINK_REPORTS_CACHE_DIR="$cache_dir"
-export PLINK_REPORTS_USE_CACHED_PLINK_HAP_ASSOC="On"
-export PLINK_REPORTS_USE_CACHED_PLINK_EXTRA_INFO="On"
+export PLINK_REPORTS_USE_CACHED_PLINK_EXTRA_INFO="Off"
 
 export PLINK_REPORTS_PLINK_REGION="9:98318926-105545483"
 # PLINK region from chr9 linkage study is from rs4077800 to rs2000182
