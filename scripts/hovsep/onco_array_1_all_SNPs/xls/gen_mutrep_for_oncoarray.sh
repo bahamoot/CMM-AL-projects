5ce9983fd7bd7f245c478980c6c9d8dbcbac4908
Jessada <jessada.thutkawkorapin@ki.se>
2015-12-18 16:01:32 +0100

    done scripts to annotate all SNPs from oncoarray genotyping data

---
+++ scripts/hovsep/onco_array_1_all_SNPs/xls/gen_mutrep_for_oncoarray.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+ 
+#SBATCH -A b2011158
+#SBATCH -p core
+#SBATCH -n 1
+#SBATCH -t 2-00:00:00
+#SBATCH -J mtr_mutrep_onco_array
+
+dataset_name="onco_array"
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cmd="pyCMM-mutrep-mutation-reports"
+cmd+=" -j $CMM_CLIENT/scripts/hovsep/onco_array/xls/$dataset_name"_jobs_setup.txt
+cmd+=" -l /proj/b2011117/private/projects/slurm_log/$dataset_name"
+eval "$cmd"
