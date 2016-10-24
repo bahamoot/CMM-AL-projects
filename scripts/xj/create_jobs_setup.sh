43651fc0746fa5936a08715167cd59737f8ccbf9
Jessada <jessada.thutkawkorapin@ki.se>
2016-10-24 14:15:33 +0200

    wrap up xj branch

--- scripts/xj/create_jobs_setup.sh
+++ scripts/xj/create_jobs_setup.sh
@@ -3,37 +3,48 @@
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 dataset_name="xj"
 project_out_dir="$MUTATIONS_REPORTS_OUTPUT_DIR/xj/"
-vcf_tabix_file="/glob/jessada/private/master_data/CMM/CMM_Axeq_b2012247/Axeq_chr3_6_14_18.vcf.gz"
-vcf_region="6"
 project_code="b2011097"
 annotated_vcf_tabix="$ANNOTATED_AXEQ_CHR3_6_14_18"
+#sample_infos="$script_dir/chr9.sample_infos"
+anno_excl_tags="Mutstat_details"
+anno_excl_tags+=",ExAC_Other"
+anno_excl_tags+=",Unknown"
 report_regions="6,18"
+#call_info="True"
 split_chrom=True
-call_info="True"
+#filter_actions="Rare"
+#filter_actions+=",Has-Mutation"
+#filter_actions+=",Has-Shared"
 #freq_ratios="ExAC_ALL:0.1"
 jobs_setup_file="$dataset_name"_jobs_setup.txt
 
 
-cmd="pyCMM-cmmdb-create-job-setup-file"
+cmd="pyCMM-mutrep-create-job-setup-file"
 cmd+=" -d $dataset_name"
 cmd+=" -O $project_out_dir"
-cmd+=" -i $vcf_tabix_file"
-cmd+=" -r $vcf_region"
 if [ ! -z $project_code ]
 then
     cmd+=" -p $project_code"
+fi
+if [ ! -z $anno_excl_tags ]
+then
+    cmd+=" -E $anno_excl_tags"
+fi
+if [ ! -z "$filter_actions" ]
+then
+    cmd+=" --filter_actions $filter_actions"
 fi
 if [ ! -z $annotated_vcf_tabix ]
 then
     cmd+=" -A $annotated_vcf_tabix"
 fi
-if [ ! -z $report_regions ]
-then
-    cmd+=" -R $report_regions"
-fi
 if [ ! -z $sample_infos ]
 then
     cmd+=" -s $sample_infos"
+fi
+if [ ! -z $report_regions ]
+then
+    cmd+=" -R $report_regions"
 fi
 if [ "$call_detail" == "True" ]
 then
@@ -47,13 +58,17 @@ if [ "$split_chrom" == "True" ]
 then
     cmd+=" --split_chrom"
 fi
-if [ "$summary_families" == "True" ]
+if [ "$summary_families" = "True" ]
 then
     cmd+=" --summary_families"
 fi
-if [ "$exclude_common" = "True" ]
+if [ "$only_families" = "True" ]
 then
-    cmd+=" --exclude_common"
+    cmd+=" --only_families"
+fi
+if [ "$only_summary" = "True" ]
+then
+    cmd+=" --only_summary"
 fi
 cmd+=" -o $jobs_setup_file"
 
--- scripts/xj/gen_muts_reps_for_6_18q_axeq.sh
+++ scripts/xj/gen_muts_reps_for_6_18q_axeq.sh
@@ -1,13 +1,7 @@
-#!/bin/bash -l
+#!/bin/bash
  
-#SBATCH -A b2011097
-#SBATCH -p core
-#SBATCH -n 1
-#SBATCH -t 1-00:00:00
-#SBATCH -J mntr_xj_6_18q_axq
-
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cmd="pyCMM-mutrep-mutation-reports"
+cmd="pyCMM-mutrep-pipeline"
 cmd+=" -j $CMM_CLIENT/scripts/xj/xj_jobs_setup.txt"
-#cmd+=" -l /proj/b2011117/private/projects/out/xj/18q_axq/18q_axq"
+#cmd+=" -l /proj/b2011117/private/projects/slurm_log/$dataset_name"
 eval "$cmd"
