68ef053cf64501864020a3ee3d119682bbd71c39
Jessada <jessada.thutkawkorapin@ki.se>
2017-04-12 13:14:36 +0200

    prelim of mutation report for TYRCA project

---
+++ scripts/TYRCA/families/gen_mutrep.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ 
+dataset_name="TYRCA"
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cmd="pyCMM-mutrep-pipeline"
+cmd+=" -j $script_dir/$dataset_name"_jobs_setup.txt
+cmd+=" -t 2-00:00:00"
+#cmd+=" -l /proj/b2011117/private/projects/slurm_log/$dataset_name"
+eval "$cmd"
