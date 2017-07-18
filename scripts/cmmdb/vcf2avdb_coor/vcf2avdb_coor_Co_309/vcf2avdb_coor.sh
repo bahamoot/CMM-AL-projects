69e6102ad19e00ddbdf149bbe609625edd40865c
Jessada <jessada.thutkawkorapin@ki.se>
2017-07-18 15:42:12 +0200

    add script to convert vcf coordinate to annovar coordinate

---
+++ scripts/cmmdb/vcf2avdb_coor/vcf2avdb_coor_Co_309/vcf2avdb_coor.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# exit on error
+set -e
+set -o pipefail
+
+source $PYCMM/bash/cmm_functions.sh
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+dataset_name="Co_309"
+vcf_tabix_file="$VCF_CO309_GTZ"
+project_out_dir="$VCF2AVDB_COOR_OUTPUT_DIR/vcf2avdb_coor_$dataset_name"
+data_out_dir="$project_out_dir/data_out"
+out_file="$data_out_dir/Co-309.gtz.vcf"
+
+mkdir -p "$data_out_dir"
+
+cmd="$PYCMM/bash/cmmdb_vcf2avdb_coor.sh"
+cmd+=" -i $vcf_tabix_file"
+cmd+=" -o $out_file"
+
+#info_msg "$cmd"
+
+eval_cmd "$cmd"
